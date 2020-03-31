ca434af741c14d9d73b4c625f3aeae64be6fe8e7
Kamil Balogun <kamilbalogun1@gmail.com>
2020-03-31 17:35:59 +0100

    add commonReducer function  to combines multiple reducers into a single function

--- src/data/DataStore.js
+++ src/data/DataStore.js
@@ -1,4 +1,6 @@
 import { createStore } from 'redux';
 import { ShopReducer } from './ShopReducer';
+import { CartReducer } from './CartReducer';
+import { CommonReducer } from './CommonReducer';
 
-export const SportsShopDataStore = createStore(ShopReducer);
+export const SportsShopDataStore = createStore(CommonReducer(ShopReducer, CartReducer));
