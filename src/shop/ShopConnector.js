36ef66ae765973d2a1d512f6676fb01d9e436a5f
Kamil Balogun <kamilbalogun1@gmail.com>
2020-04-01 14:51:06 +0100

    update the routing configuration in the ShopConnector component to add support for the /shop/cart URL

--- src/shop/ShopConnector.js
+++ src/shop/ShopConnector.js
@@ -5,6 +5,8 @@ import { loadData } from '../data/ActionCreators';
 import { DataTypes } from '../data/Types';
 import { Shop } from './Shop';
 import { addToCart, updateCartQuantity, removeFromCart, clearCart } from '../data/CartActionCreators';
+import { CartDetails } from './CartDetails';
+
 
 const mapStateToProps = (datastore) => ({
   ...datastore
@@ -29,6 +31,12 @@ export const ShopConnector = connect(mapStateToProps, mapDispatchToProps)(
             return <Shop { ...this.props} { ...routeProp }
               products={ filterProducts(this.props.products, routeProp.match.params.category)}
             />
+          }}
+        />
+        <Route
+          path="/shop/cart"
+          render={ (routeProp) => {
+            return <CartDetails { ...this.props} { ...routeProp }/>
           }}
         />
         <Redirect to="/shop/products" />
